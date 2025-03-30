7d346167ed0913ddfd7cbeb787c16960c9173aa0
joshuatxtcllc <jaysframes@gmail.com>
2025-03-30 10:37:30 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container footer-content">
+        <div className="
